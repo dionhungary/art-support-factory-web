16fc278fd1df75f0f4adec91d376652e9c6be42f
Balazs <100231530+morzsamx@users.noreply.github.com>
2026-05-27 12:52:52 +0200

    seo

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,17 @@
+const siteUrl = 'https://artsupportfactory.hu';
+
+export function GET() {
+	const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${siteUrl}/</loc>
+  </url>
+</urlset>
+`;
+
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8',
+		},
+	});
+}
